532a6e40bf66af1c4af3219374e660012c76881c
Syed Ali Haider <roeticvampire@users.noreply.github.com>
2021-03-01 02:27:30 +0530

    initial commit

---
+++ app/src/main/java/com/roeticvampire/fleet/IntroScreen.java
@@ -0,0 +1,14 @@
+package com.roeticvampire.fleet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_screen);
+    }
+}
