160283e29eb5622e35ebbd1d79aab70ae9027a7d
Syed Ali Haider <roeticvampire@users.noreply.github.com>
2021-03-02 02:23:37 +0530

    added loginactivity

--- app/src/main/java/com/roeticvampire/fleet/LoginActivity.java
+++ app/src/main/java/com/roeticvampire/fleet/LoginActivity.java
@@ -3,15 +3,12 @@ package com.roeticvampire.fleet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
 
+public class LoginActivity extends AppCompatActivity {
 
-public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        EditText email_input=findViewById(R.id.input_email);
-
+        setContentView(R.layout.activity_login);
     }
 }
